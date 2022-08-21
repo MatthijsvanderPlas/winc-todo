d04fb26b52922acdf2257d454fd9f0a548f3f837
matthijsvanderplas <mvdplas84@ziggo.nl>
2022-08-21 14:02:12 +0200

    Start fresh

---
+++ api-client.js
@@ -0,0 +1,65 @@
+const url = `http://localhost:3000`;
+
+export const getTodoList = async () => {
+  try {
+    const res = await fetch(url, {
+      method: 'GET',
+      headers: {
+       "Content-Type": "application/json" 
+      }
+    });
+    const data = await res.json()
+    if(res.status === 200) return { data, res }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const addTodoTask = async (body = {}) => {
+  try {
+    const res = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        "Content-type": "application/json"
+      },
+    })
+    const data = await res.json();
+    if (res.status === 200) return { data, res }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const updateTodoTask = async (todoID, body) => {
+  const urlID = url +`/${totodID}`
+  try {
+    const res = await fetch(urlID, {
+      method: 'PUT',
+      body: JSON.stringify(body),
+      headers: {
+       "Content-Type": "application/json" 
+      }
+    });
+    const data = await res.json()
+    if(res.status === 200) return { data, res}
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export const deleteTodoTask = async (todoID) => {
+  const newURL = url + `/${todoID}`
+  try {
+    const res = await fetch(newURL, {
+      method: 'DELETE',
+      headers: {
+        "Content-type": "application/json"
+      },
+    })
+    const data = await res.json();
+    if (res.status === 200) return { data, res }
+  } catch (err) {
+    console.log(err)
+  }
+}
